679b9a01c8d30de85d180a05a14856762b297259
Brusa <JBrusa@Southgate.local>
2024-01-15 18:41:50 -0500

    created login system and superuser

--- streaming_transition/urls.py
+++ streaming_transition/urls.py
@@ -1,6 +1,9 @@
 from django.urls import path
-from .views import LandingPageView
+from .views import LandingPageView, UserLoginView, UserLogoutView
+
 
 urlpatterns = [
     path('', LandingPageView.as_view(), name='landing-page'),
+    path('login/', UserLoginView.as_view(next_page='landing-page'), name='login'),
+    path('logout/', UserLogoutView.as_view(), name='logout'),
 ]
--- streaming_transition/views.py
+++ streaming_transition/views.py
@@ -1,12 +1,30 @@
-from django.shortcuts import render
+from django.shortcuts import render, redirect
+from django.urls import reverse
+
+from django.contrib.auth.mixins import LoginRequiredMixin
+from django.contrib.auth import logout
+
 from django.views.generic import TemplateView
+from django.contrib.auth.views import LoginView, LogoutView
+from django.views import View
+
 
 # Create your views here.
 
-class LandingPageView(TemplateView):
-    template_name = "base/landing.html"
+class LandingPageView(LoginRequiredMixin, TemplateView):
+    template_name = "base/landing-page.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['message'] = 'Hello, World!'
         return context
+    
+
+class UserLoginView(LoginView):
+    template_name = 'base/login.html'
+
+
+class UserLogoutView(View):
+    def get(self, request):
+        logout(request)
+        return redirect('landing-page')
