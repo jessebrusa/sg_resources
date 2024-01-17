e72bc1def24235e977e0cc0d28f76fc7379d035f
Brusa <JBrusa@Southgate.local>
2024-01-17 15:15:48 -0500

    created visit table and started homepage for app

--- streaming_transition/models.py
+++ streaming_transition/models.py
@@ -4,12 +4,32 @@ from django.db import models
 class Apartment(models.Model):
     apartment_number = models.IntegerField()
     building = models.CharField(max_length=255)
-    comments = models.TextField(null=True, blank=True)
     complete = models.BooleanField(default=False)
 
+    def __str__(self):
+        return f"{self.apartment_number} {self.building}"
+    
+
+class Visit(models.Model):
+    apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE, related_name='visits')
+    date = models.DateField()
+    time = models.TimeField()
+    technician = models.CharField(max_length=255)
+    comments = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.apartment.apartment_number}{self.apartment.building[0]} - {self.date}"
+    
 
 class Device(models.Model):
     apartment = models.ForeignKey(Apartment, on_delete=models.CASCADE, related_name='devices')
+    visit = models.ForeignKey(Visit, on_delete=models.CASCADE, related_name='devices')
     device_type = models.CharField(max_length=255)
     model = models.CharField(max_length=255)
+    built_in = models.CharField(max_length=255, null=True, blank=True, default=None)
     smart_device = models.CharField(max_length=255, null=True, blank=True)
+    comments = models.TextField(null=True, blank=True)
+    reccomendation = models.TextField(null=True, blank=True)
+
+    def __str__(self):
+        return f"{self.apartment.apartment_number}{self.apartment.building[0]} - {self.device_type}: {self.model}"
--- streaming_transition/views.py
+++ streaming_transition/views.py
@@ -1,11 +1,23 @@
 from django.contrib.auth.mixins import LoginRequiredMixin
 from django.views.generic import TemplateView
+from .models import Apartment
 
-
-class StreamingTransitionHomeView(LoginRequiredMixin, TemplateView):
+class StreamingTransitionHomeView(TemplateView):  # replace with your actual view class name
     template_name = "base/streaming-transition-home.html"
 
     def get_context_data(self, **kwargs):
         context = super().get_context_data(**kwargs)
         context['username'] = self.request.user.username
+
+        total_apartments = Apartment.objects.count()
+        completed_apartments = Apartment.objects.filter(complete=True).count()
+
+        if total_apartments > 0:
+            percent_completed = (completed_apartments / total_apartments) * 100
+            percent_completed = round(percent_completed, 2)
+        else:
+            percent_completed = 0
+
+        context['percent_completed'] = percent_completed
+
         return context
