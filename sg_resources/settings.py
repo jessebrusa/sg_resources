4094ec8401a340a0f94e8b67feda72b4da8e619f
Brusa <JBrusa@Southgate.local>
2024-01-25 14:59:20 -0500

    ready for deployment v2

--- sg_resources/settings.py
+++ sg_resources/settings.py
@@ -2,7 +2,7 @@ from pathlib import Path
 
 from dotenv import load_dotenv
 import os
-load_dotenv()
+
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = Path(__file__).resolve().parent.parent
@@ -12,10 +12,10 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/5.0/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = os.getenv('SECRET_KEY')
+SECRET_KEY = "uDPB3j@87evCfK$NH21mqbMS@nHm4UGBbc6qeK9k1@3fKvmQudJ$Xz8Q3cD$"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = os.getenv('DEBUG')
+DEBUG = True
 
 ALLOWED_HOSTS = ['*']
 
